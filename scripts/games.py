1c5a22c0e4ecea764de29e1c3e4f937da804b81f
Daniel Dias <danielbpdias@gmail.com>
2022-06-07 17:14:50 -0300

    Added random_player and query_player

--- scripts/games.py
+++ scripts/games.py
@@ -1,6 +1,7 @@
 ## Algorithms shown in AIMA, extracted from: https://github.com/aimacode/aima-python/blob/master/games.py
 
 from collections import namedtuple
+import random
 
 GameState = namedtuple('GameState', 'to_move, utility, board, moves')
 
@@ -132,3 +133,24 @@ class ConnectFour(TicTacToe):
     def actions(self, state):
         return [(x, y) for (x, y) in state.moves
                 if x == self.h or (x + 1 , y ) in state.board]
+
+def random_player(game, state):
+    """A player that chooses a legal move at random."""
+    return random.choice(game.actions(state)) if game.actions(state) else None
+
+def query_player(game, state):
+    """Make a move by querying standard input."""
+    print("current state:")
+    game.display(state)
+    print("available moves: {}".format(game.actions(state)))
+    print("")
+    move = None
+    if game.actions(state):
+        move_string = input('Your move? ')
+        try:
+            move = eval(move_string)
+        except NameError:
+            move = move_string
+    else:
+        print('no legal moves: passing turn to next player')
+    return move
